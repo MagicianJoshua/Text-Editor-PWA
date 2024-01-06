8a10972425f537e5277fe821a2d4d3a92b4de54a
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-06 00:03:47 -0400

    Got index db working but its not saving on reload, need to fix

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,42 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  console.log("Putting to Jate");
+
+  const JateDb = await openDB("jate", 1);
+
+  const transaction = JateDb.transaction("jate", "readwrite");
+
+  const store = transaction.objectStore("jate");
+
+  const request = store.add({ value: content });
+
+  const result = await request;
+
+  console.log("Data saved to the database", result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  
+  console.log("GET all from the database");
+
+  const JateDb = await openDB("jate", 1);
+
+  const transaction = JateDb.transaction("jate", "readonly");
+
+  const store = transaction.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log("Getting from database", result);
+
+  return result;
+};
 
 initdb();
