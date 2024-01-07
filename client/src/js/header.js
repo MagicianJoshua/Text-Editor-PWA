dc57cb475ede5e16eadf6c130432cb7fadafb005
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-06 22:31:28 -0400

    I really dont know why this isnt building, something to do with the header

--- client/src/js/header.js
+++ client/src/js/header.js
@@ -1,3 +1,12 @@
-const header
+const header = `
+/*
+       _____  ____________
+      / /   |/_  __/ ____/
+ __  / / /| | / / / __/   
+/ /_/ / ___ |/ / / /___   
+ ____/_/  |_/_/ /_____/   
+just another text editor
+*/                          
+`;
 
 export { header };
